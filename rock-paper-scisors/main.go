66645c8285c0c3de558a8c07b6812c9c1437f526
Pavel <bamboo2panda@gmail.com>
2022-01-06 01:17:48 +0300

    a

--- rock-paper-scisors/main.go
+++ rock-paper-scisors/main.go
@@ -21,29 +21,108 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	playerChoice := ""
 	playerValue := -1
-
-	computerValue := rand.Intn(2)
+	playerScore := 0
+	computerScore := 0
 
 	reader := bufio.NewReader(os.Stdin)
 
-	clearScreen()
+	fmt.Println("Rock, Paper & Scissors")
+	fmt.Println("----------------------")
+	fmt.Println("Game is played for 3 rounds, and best of three wins the game. Good luck!")
+	fmt.Println()
 
-	fmt.Print("Please enter rock, paper, or scissors ->")
-	playerChoice, _ = reader.ReadString('\n')
+	for i := 1; i <= 3; i++ {
 
-	playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+		fmt.Println("Round", i)
+		fmt.Println("--------")
 
-	if playerChoice == "rock" {
-		playerValue = ROCK
-	} else if playerChoice == "paper" {
-		playerValue = PAPER
-	} else if playerChoice == "scissors" {
-		playerValue = SCISSORS
+		fmt.Print("Please enter rock, paper, or scissors ->")
+		playerChoice, _ = reader.ReadString('\n')
+
+		playerChoice = strings.Replace(playerChoice, "\n", "", -1)
+
+		computerValue := rand.Intn(2)
+
+		if playerChoice == "rock" {
+			playerValue = ROCK
+		} else if playerChoice == "paper" {
+			playerValue = PAPER
+		} else if playerChoice == "scissors" {
+			playerValue = SCISSORS
+		} else {
+			playerValue = -1
+		}
+
+		fmt.Println()
+		fmt.Println("Player choice", strings.ToUpper(playerChoice))
+
+		switch computerValue {
+		case ROCK:
+			fmt.Println("Computer prints ROCK")
+			break
+		case PAPER:
+			fmt.Println("Computer chose PAPER")
+			break
+		case SCISSORS:
+			fmt.Println("Computer chose SCISSORS")
+			break
+		default:
+		}
+		fmt.Println()
+
+		if playerValue == computerValue {
+			fmt.Println("It`s a draw")
+			i--
+		} else {
+			switch playerValue {
+			case ROCK:
+				if computerValue == PAPER {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			case PAPER:
+				if computerValue == SCISSORS {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			case SCISSORS:
+				if computerValue == ROCK {
+					computerScore = computerWins(computerScore)
+				} else {
+					playerScore = playerWins(playerScore)
+				}
+				break
+			default:
+				fmt.Println("Invalid choise!")
+			}
+		}
 	}
 
+	fmt.Println("Final score")
+	fmt.Println("------------")
+	fmt.Printf("Player: %d/3, Computer %d/3", playerScore, computerScore)
 	fmt.Println()
-	fmt.Println("Player chose", playerChoice, "and value is", playerValue)
-	fmt.Println("Computer chose", computerValue)
+
+	if playerScore > computerScore {
+		fmt.Println("Player wins game!")
+	} else {
+		fmt.Println("Computer wins game!")
+	}
+
+}
+
+func computerWins(score int) int {
+	fmt.Println("Computer wins!")
+	return score + 1
+}
+
+func playerWins(score int) int {
+	fmt.Println("Player Wins!")
+	return score + 1
 }
 
 // clearScreen clears the screen
