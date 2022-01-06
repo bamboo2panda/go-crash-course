5e56a89201d6cd20b5c2811a91949e53d34a545f
Pavel <bamboo2panda@gmail.com>
2022-01-07 00:46:37 +0300

    Assigment operators

--- operators/main.go
+++ operators/main.go
@@ -1,27 +1,13 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	a := 12
-	b := 6
+	x := 12
+	x--
 
-	c, err := devideTwoNumbers(a, b)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		if c == 2 {
-			fmt.Println("We found 2!")
-		}
-	}
-}
-
-func devideTwoNumbers(x, y int) (int, error) {
-	if y == 0 {
-		return 0, errors.New("can not devide by zero")
-	}
-	return x / y, nil
+	fmt.Println(x)
+	y := 10
+	y /= 2
+	fmt.Println(y)
 }
