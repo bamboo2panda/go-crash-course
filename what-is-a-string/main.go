dec65dceb1fcc6894cc121482de2eb9febc7b1b8
Pavel <bamboo2panda@gmail.com>
2022-01-07 13:19:02 +0300

    String length

--- what-is-a-string/main.go
+++ what-is-a-string/main.go
@@ -4,16 +4,19 @@ import "fmt"
 
 func main() {
 	courseName := "Learn Go for Beginners Crash course"
-	fmt.Println(string(courseName[0]))
-	fmt.Println(string(courseName[6]))
 
 	for i := 0; i <= 21; i++ {
 		fmt.Println(string(courseName[i]))
 	}
 	fmt.Println()
 
-	for i := 13; i <= 21; i++ {
-		fmt.Println(string(courseName[i]))
-	}
-	fmt.Println()
+	fmt.Println("Length of courseName is", len(courseName))
+
+	var mySlice []string
+	mySlice = append(mySlice, "one")
+	mySlice = append(mySlice, "two")
+	mySlice = append(mySlice, "three")
+
+	fmt.Println("mySlice has", len(mySlice), "elements")
+	fmt.Println("the last element in mySlice is", mySlice[len(mySlice)-1])
 }
