67f6aa771cbd737cafacab7e206e151cab3bd762
Pavel <bamboo2panda@gmail.com>
2022-01-07 13:12:25 +0300

    Strings indexing

--- what-is-a-string/main.go
+++ what-is-a-string/main.go
@@ -1,59 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	fmt.Println()
-	name := "Hello world"
-	fmt.Println("String:", name)
-	fmt.Println()
-	fmt.Println("Bytes")
-	for i := 0; i < len(name); i++ {
-		fmt.Printf("%x ", name[i])
-	}
-	fmt.Println()
-	fmt.Println()
+	courseName := "Learn Go for Beginners Crash course"
+	fmt.Println(string(courseName[0]))
+	fmt.Println(string(courseName[6]))
 
-	fmt.Println("Index\tRune\tString")
-	for x, y := range name {
-		fmt.Println(x, "\t", y, "\t", string(y))
+	for i := 0; i <= 21; i++ {
+		fmt.Println(string(courseName[i]))
 	}
 	fmt.Println()
 
-	name = "Γειά σου Κόσμε"
-	fmt.Println("Index\tRune\tString")
-	for x, y := range name {
-		fmt.Println(x, "\t", y, "\t", string(y))
+	for i := 13; i <= 21; i++ {
+		fmt.Println(string(courseName[i]))
 	}
 	fmt.Println()
-
-	fmt.Println()
-	fmt.Println("Tree ways to concatinate strings")
-
-	h := "Hello "
-	w := "World."
-
-	// using + not very efficient
-	mystring := h + w
-	fmt.Println(mystring)
-	fmt.Println()
-
-	// using fmt - more efficient
-	mystring = fmt.Sprintf("%s%s", h, w)
-	fmt.Println(mystring)
-	fmt.Println()
-
-	// using stringbuilder - very efficient
-	var sb strings.Builder
-	sb.WriteString(h)
-	sb.WriteString(w)
-	fmt.Println(sb.String())
-
-	fmt.Println()
-	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	fmt.Println("Getting substring")
-	fmt.Println(name[1:13])
 }
